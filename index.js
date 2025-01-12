52d988fea6b247dd02140179b45583e9d128a851
manoj1995madushanka <madushankamanoj414@gmail.com>
2025-01-13 00:20:44 +0530

    improve file generation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const { PDFDocument } = require('pdf-lib');
 
 // Initialize Secrets Manager
 const secretsManager = new AWS.SecretsManager();
@@ -45,13 +47,47 @@ exports.handler = async (event) => {
       },
     });
 
+    // Get current date and month
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString('en-US');
+    const currentMonth = currentDate.toLocaleString('en-US', { month: 'long' });
+    const year = currentDate.getFullYear();
+
+    // Load the template PDF
+    const templatePdfBytes = fs.readFileSync('/template.pdf'); // Replace with the actual path
+    const pdfDoc = await PDFDocument.load(templatePdfBytes);
+
+    // Get the first page of the PDF
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+
+    // Replace text (adjust according to template)
+    // coordinate start at left bottom corner
+    firstPage.drawText(formattedDate, { x: 100, y: 500, size: 12 });
+    firstPage.drawText(`month of ${currentMonth}`, { x: 100, y: 480, size: 12 });
+
+    // Save the modified PDF
+    const fileName = `Invoice-${currentMonth}-2025-Manoj-Madushanka.pdf`;
+    const modifiedPdfBytes = await pdfDoc.save();
+
+    const bodyContent = `Hi,
+      Please find the attached Invoice for the month of ${currentMonth} ${year}.
+      Best Regards,
+      Manoj Madushanka.`;
+
     // Email options
     const mailOptions = {
       from: 'fancycrazelk@gmail.com',
       to: 'madushankamanoj414@gmail.com',
-      subject: 'Monthly Reminder',
-      text: 'This is your monthly email reminder!',
-      html: '<p>This is your <strong>monthly email reminder</strong>!</p>',
+      subject: 'Invoice-January-2025-Manoj-Madushanka',
+      text: bodyContent,
+      html:`<p>${bodyContent}</p>`,
+      attachments: [
+        {
+          filename: fileName, // File name to display in the email
+          content: modifiedPdfBytes, // File content as a buffer
+        },
+      ],
     };
 
     // Send email
